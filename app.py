b7dacdc218bde670f49828513d9db01ea449c6fb
Barrett Bryson <bryson.barrett@gmail.com>
2018-10-19 09:36:16 -0400

    Found a more applicable tutorial to follow, files have changed a lot

---
+++ app.py
@@ -0,0 +1,58 @@
+import RPi.GPIO as GPIO
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+GPIO.setmode(GPIO.BCM)
+
+#Dictionary that holds pin number and state
+pins = {
+    23 : {'name' : 'GPIO 23', 'state' : GPIO.LOW},
+    24 : {'name' : 'GPIO 24', 'state' : GPIO.LOW}
+    }
+    
+#Set each pin to output mode and lower
+for pin in pins:
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, pin.get('state'))
+    
+@app.route('/')
+def main():
+    #Read pin state and store in the dict
+    for pin in pins:
+        pins[pin]['state'] = GPIO.input(pin)
+        
+    templateData = {
+        'pins' : pins
+    }
+    
+    #pass pin data to the html file
+    return render_template('main.html',**templateData)
+ 
+@app.route('/<changePin>/<action>')
+def action(changePin, action):
+    changePin = int(changePin)
+    deviceName = pins[changePin]['name']
+    #if action in URL is on then
+    if action == 'on':
+        GPIO.output(changePin, GPIO.HIGH)
+        message = "Turned " + deviceName + " on."
+        
+    if action == 'off':
+        GPIO.output(changePin,GPIO.LOW)
+        message = "Turned " + deviceName + " off."
+        
+    #update states
+    for pin in pins:
+        pins[pin]['state'] = GPIO.input(pin)
+    
+    templateData = {
+        'pins' : pins
+    }
+    
+    #pass pin data to the html file
+    return render_template('main.html',**templateData)
+   
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80, debug=True)
+  
