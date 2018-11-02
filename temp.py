e279056f9f3d6558b0e64d75bd32b8b3ffc9481b
BarrettJB <bryson.barrett@gmail.com>
2018-11-02 09:25:32 -0500

    small tweak to temp.py

--- temp.py
+++ temp.py
@@ -9,14 +9,16 @@ print('Reading out temperature, resistance, and voltage values')
 print('|  Temp  |   R    |  Volt  |')
 
 T0 = 298.15
+Tk = 273.15
 B = 3950
 R0 = 10000
 
 while True:
-    num = adc.read(0,gain=GAIN)
-    volt = num*(5/32768)
+    num = adc.read_adc(0,gain=GAIN)
+    volt = num*(5.0/32768)
     Rt = (volt*10000)/(5-volt)
-    T_inv = (1/T0) + (1/B)*log(Rt/10000)
-    T = 1/T_inv
-    print('|  %2.2f  | %5.1f | %1.5f |' % (T,Rt,volt))
+    T_inv = (1.0/T0) + (1.0/B)*log(Rt/10000)
+    T = (1.0/T_inv)-Tk
+    print('|  %2.2fC | %5.1f | %1.5f |' % (T,Rt,volt))
+    time.sleep(0.5)
     
